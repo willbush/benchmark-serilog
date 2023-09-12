d8a54c5779a6f2ba6adee84c291ef2e3006977ad
Will Bush <will.g.bush@gmail.com>
2023-09-12 02:07:48 -0500

    Add benchmarks and refactor

--- Program.cs
+++ Program.cs
@@ -9,12 +9,123 @@ using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace benchmark_serilog;
 
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
+
 public class Program
 {
+    public const string Message = "Keyboard not found. Press F1 to continue.";
+    public static readonly Point Point = new() { X = 1, Y = 2 };
+
     static void Main(string[] args)
-    {
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-    }
+        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
+
+[MemoryDiagnoser]
+[SuppressMessage("Performance", "CA1822:Mark members as static")] // benchmarks must be non-static
+public class NoSinkBenchmarks
+{
+    // Default ILogger (i.e. Microsoft.Extensions.Logging.ILogger).
+    static readonly ILogger<NoSinkBenchmarks> Logger =
+        LoggerFactory.Create(_ => { }).CreateLogger<NoSinkBenchmarks>();
+
+    // SeriLog Logger.
+    static readonly Logger SeriLogger = new LoggerConfiguration().CreateLogger();
+
+    // SeriLog via default ILogger using compile-time logging source generation.
+    static readonly ILogger<NoSinkBenchmarks> SeriDefaultLogger =
+        new SerilogLoggerFactory().CreateLogger<NoSinkBenchmarks>();
+
+    [Benchmark]
+    public void Default() => Logger.LogInformation(Program.Message);
+
+    [Benchmark]
+    public void Serilog() => SeriLogger.Information(Program.Message);
+
+    [Benchmark]
+    public void Default_CodeGen_Something() => Logger.LogSomething(Program.Message, 42);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Something() => SeriDefaultLogger.LogSomething(Program.Message, 42);
+
+    [Benchmark]
+    public void Default_CodeGen_Info() => Logger.Info(Program.Message);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Info() => SeriDefaultLogger.Info(Program.Message);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Point() => SeriDefaultLogger.LogPoint(Program.Message, Program.Point);
+}
+
+[MemoryDiagnoser]
+[SuppressMessage("Performance", "CA1822:Mark members as static")] // benchmarks must be non-static
+public class ConsoleBenchmarks
+{
+    // Default. Console logging.
+    static readonly ILogger<ConsoleBenchmarks> Logger =
+        LoggerFactory.Create(b => b.AddConsole()).CreateLogger<ConsoleBenchmarks>();
+
+    // SeriLog. Console logging.
+    static readonly Logger SeriLogger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+    // SeriLog. Console logging with Compile-time logging source generation.
+    static readonly ILogger<ConsoleBenchmarks> SeriDefaultLogger =
+        new SerilogLoggerFactory().CreateLogger<ConsoleBenchmarks>();
+
+    // SeriLog Logger with async console sink.
+    static readonly Logger SeriLoggerAsync =
+        new LoggerConfiguration().WriteTo.Async(x => x.Console()).CreateLogger();
+
+    // SeriLog Logger with async console sink.
+    static readonly ILogger<ConsoleBenchmarks> SeriDefaultLoggerAsync =
+        new SerilogLoggerFactory(
+            new LoggerConfiguration().WriteTo.Async(x => x.Console()).CreateLogger())
+            .CreateLogger<ConsoleBenchmarks>();
+
+    [Benchmark]
+    public void Default() => Logger.LogInformation(Program.Message);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Point() => Logger.LogPoint(Program.Message, Program.Point);
+
+    [Benchmark]
+    public void Serilog() => SeriLogger.Information(Program.Message);
+
+    [Benchmark]
+    public void Serilog_Async() => SeriLoggerAsync.Information(Program.Message);
+
+    [Benchmark]
+    public void Default_CodeGen_Something() => Logger.LogSomething(Program.Message, 42);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Something() => SeriDefaultLogger.LogSomething(Program.Message, 42);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Something_Async() => SeriDefaultLoggerAsync.LogSomething(Program.Message, 42);
+
+    [Benchmark]
+    public void Default_CodeGen_Info() => Logger.Info(Program.Message);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Info() => SeriDefaultLogger.Info(Program.Message);
+
+    [Benchmark]
+    public void Serilog_CodeGen_Info_Async() => SeriDefaultLoggerAsync.Info(Program.Message);
+
+    [Benchmark]
+    public void Serilog_Point() => SeriLogger.Information("{Message} {@Point}", Program.Message, Program.Point);
+
+    [Benchmark]
+    public void Serilog_Point_Async() => SeriDefaultLoggerAsync.LogInformation("{Message} {@Point}", Program.Message, Program.Point);
 }
 
 public static partial class Logging
@@ -30,84 +141,10 @@ public static partial class Logging
         Level = LogLevel.Information,
         Message = "{message} {n}")]
     public static partial void LogSomething(this ILogger logger, string message, int n);
-}
 
-[MemoryDiagnoser]
-[SuppressMessage("Performance", "CA1822:Mark members as static")] // benchmarks must be non-static
-public class NoSinkBenchmarks
-{
-    const string Message = "Keyboard not found. Press F1 to continue.";
-
-    // Default ILogger (i.e. Microsoft.Extensions.Logging.ILogger).
-    static readonly ILogger<NoSinkBenchmarks> Logger = LoggerFactory.Create(_ => { }).CreateLogger<NoSinkBenchmarks>();
-
-    // SeriLog Logger.
-    static readonly Logger SeriLogger = new LoggerConfiguration().CreateLogger();
-
-    // SeriLog via default ILogger using compile-time logging source generation.
-    static readonly ILogger<NoSinkBenchmarks> SeriDefaultLogger = new SerilogLoggerFactory().CreateLogger<NoSinkBenchmarks>();
-
-    [Benchmark]
-    public void Default() => Logger.LogInformation(Message);
-
-    [Benchmark]
-    public void Serilog() => SeriLogger.Information(Message);
-
-    [Benchmark]
-    public void Default_CodeGen_Something() => Logger.LogSomething(Message, 42);
-
-    [Benchmark]
-    public void Serilog_CodeGen_Something() => SeriDefaultLogger.LogSomething(Message, 42);
-
-    [Benchmark]
-    public void Default_CodeGen_Info() => Logger.Info(Message);
-
-    [Benchmark]
-    public void Serilog_CodeGen_Info() => SeriDefaultLogger.Info(Message);
-}
-
-[MemoryDiagnoser]
-[SuppressMessage("Performance", "CA1822:Mark members as static")] // benchmarks must be non-static
-public class ConsoleBenchmarks
-{
-    const string Message = "This statement is false.";
-
-    // Default. Console logging.
-    static readonly ILogger<ConsoleBenchmarks> Logger = LoggerFactory.Create(b => b.AddConsole()).CreateLogger<ConsoleBenchmarks>();
-
-    // SeriLog. Console logging.
-    static readonly Logger SeriLogger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-
-    // SeriLog. Console logging with Compile-time logging source generation.
-    static readonly ILogger<ConsoleBenchmarks> SeriDefaultLogger = new SerilogLoggerFactory().CreateLogger<ConsoleBenchmarks>();
-
-    // SeriLog Logger with async console sink.
-    static readonly Logger SeriLoggerAsync = new LoggerConfiguration().WriteTo.Async(x => x.Console()).CreateLogger();
-
-    // SeriLog Logger with async console sink.
-    static readonly ILogger<ConsoleBenchmarks> SeriDefaultLoggerAsync = new SerilogLoggerFactory(new LoggerConfiguration().WriteTo.Async(x => x.Console()).CreateLogger()).CreateLogger<ConsoleBenchmarks>();
-
-    [Benchmark]
-    public void Default() => Logger.LogInformation(Message);
-
-    [Benchmark]
-    public void Serilog() => SeriLogger.Information(Message);
-
-    [Benchmark]
-    public void Serilog_Async() => SeriLoggerAsync.Information(Message);
-
-    [Benchmark]
-    public void Default_CodeGen_Something() => Logger.LogSomething(Message, 42);
-
-    [Benchmark]
-    public void Serilog_CodeGen_Something() => SeriDefaultLogger.LogSomething(Message, 42);
-
-    [Benchmark]
-    public void Serilog_CodeGen_Something_Async() => SeriDefaultLoggerAsync.LogSomething(Message, 42);
-
-    [Benchmark]
-    public void Default_CodeGen_Info() => Logger.Info(Message);
-
-    [Benchmark]
-    public void Serilog_CodeGen_Info() => SeriDefaultLogger.Info(Message);
+    [LoggerMessage(
+        EventId = 2,
+        Level = LogLevel.Information,
+        Message = "{message} {point}")]
+    public static partial void LogPoint(this ILogger logger, string message, Point point);
 }
